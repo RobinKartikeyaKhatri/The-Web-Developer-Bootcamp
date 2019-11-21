bf8e0a57338a3c57f23f02e570c1806fa266adb3
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-11-21 10:31:23 +0530

    Learned about JS Variables

---
+++ introToJavaScript/jsVariables.js
@@ -0,0 +1,23 @@
+var name = "Robin";
+console.log(name);
+
+name = "Lalit";
+console.log(name);
+
+let myFullName = "Robin Kartikeya Khatri";
+console.log(myFullName);
+
+const petName = "Bozoo";
+console.log(petName);
+
+const secretNumber = 8;
+console.log(secretNumber);
+
+let isAdorable = true;
+console.log(isAdorable);
+
+let num = 37;
+console.log(num + 3 + 40);
+
+let brother = "Rahul";
+console.log("Hello " + brother);
