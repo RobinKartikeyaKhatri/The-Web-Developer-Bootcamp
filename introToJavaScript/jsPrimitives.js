133f9f3d2a027cd1cbc8bb953dd4a79bb0500d67
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-11-21 10:03:12 +0530

    Learned JavaScript's Primitive Data Types

---
+++ introToJavaScript/jsPrimitives.js
@@ -0,0 +1,28 @@
+// Install Node and execute this file with node <file_name> command
+
+
+// Numbers
+console.log(5 + 4);
+console.log(3 - 12);
+console.log((7 * 3) + 3 - 8);
+console.log(1 / 3);
+console.log(10 % 3);
+console.log(21 % 3);
+console.log(22 % 3);
+console.log(15 % 11);
+
+
+// Strings
+console.log('Hello');
+console.log('Hello World');
+console.log("Hello Robin");
+console.log("Good" + " " + "morning" + " " + "Robin!");
+console.log("Who said, \"Give me Red!!!\" - \"Netaji Subhash Chandra Bose\"");
+console.log('Who said, ' + '"Give me Red!!!"' + ' - "Netaji Subhash Chandra Bose"');
+console.log(`Who said, Give me Red!!! - Netaji Subhash Chandra Bose`);
+console.log("My name is Robin\t Khatri I am from India");
+console.log("Hello World" .length);
+console.log(`ABC`.length);
+console.log('Robin'[0]);
+console.log(`Robin`[1]);
+console.log("Robin"[4]);
