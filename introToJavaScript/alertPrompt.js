283d5c68e750190009c70f6fcb65eea189735bdc
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-11-21 11:14:07 +0530

    Learned about alert(), prompt(), and console.log() methods

---
+++ introToJavaScript/alertPrompt.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+
+console.log(typeof "Robin");
+
+var sum = 5 + 5;
+
+alert(sum);
+console.log(typeof sum);
+
+alert(typeof (5 + 5));
+
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+
+alert("Your full name is " + firstName + " " + lastName);
