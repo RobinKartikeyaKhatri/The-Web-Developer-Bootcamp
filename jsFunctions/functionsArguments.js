b3b26caf369b7f89a9c99a2a249e58422ac03b47
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-11-22 14:03:50 +0530

    Learned how to define and call functions that take arguments

---
+++ jsFunctions/functionsArguments.js
@@ -0,0 +1,21 @@
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(5, 10);
+
+const multiply = function(num1, num2) {
+    console.log(num1 * num2);
+}
+
+multiply(2, 3);
+
+const square = (num) => {
+    console.log(num * num);
+}
+
+square(3);
+
+const square2 = num => console.log(num * num);
+
+square2(12);
