d03682326d370c5fee9999b62e75232e0f980cc6
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-11-22 14:36:44 +0530

    Learned how to use return statement in functions

---
+++ jsFunctions/functionsWithReturnStatements.js
@@ -0,0 +1,34 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+console.log("Addition of two numers are: " + add(5, 10));
+
+
+
+
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+}
+
+const result = multiply(2, 3);
+
+console.log("Multiplication of two numbers are: " + result);
+
+
+
+
+
+const square = num => {
+    return num * num;
+}
+
+console.log("Square of two numbers are: " + square(3));
+
+
+
+
+const sub = num => (num, num);
+
+console.log("Subtraction of two numers are: " + sub(12 - 2));
