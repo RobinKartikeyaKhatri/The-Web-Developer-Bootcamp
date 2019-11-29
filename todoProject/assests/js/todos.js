5e8177e6507391fbd0bde93e29e9fd190910228f
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-11-29 15:35:17 +0530

    Implement todo creation feature

--- todoProject/assests/js/todos.js
+++ todoProject/assests/js/todos.js
@@ -1,12 +1,21 @@
 // Check off Specific Todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(300, function () {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+// Add todo
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        var todoText =  $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
