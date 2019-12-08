5675fc9910ae7f2fe2985cfc7fa2b9e53867da2b
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-08 08:35:05 +0530

    Learned how to use Conditionals and Loops in EJS

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -10,6 +10,18 @@ app.get("/fallinlovewith/:thing", function (req, res) {
     res.render("love.ejs", {thingvar: thing});
 });
 
+app.get("/posts", function(req, res) {
+    var posts = [
+        {title: "My cute cat Coco", author: "Susy"},
+        {title: "My adorable dog Boozo", author: "Robin"},
+        {title: "All time favorite PS4", author: "Raja"},
+        {title: "I like winters", author: "Lalit"},
+        {title: "Jay Shree Krishna", author: "Avinash"}
+    ];
+
+    res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(3000, function() {
     console.log("Server started successfully");
 });
