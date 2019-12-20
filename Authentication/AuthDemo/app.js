04e3394bc1c0165417b8937b4ef571da44e492fb
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-20 15:33:06 +0530

    Create middleware to handle login functionality

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -3,7 +3,7 @@ mongoose                = require("mongoose"),
 passport                = require("passport"),
 bodyParser              = require("body-parser"),
 User                    = require("./models/user"),
-localStrategy           = require("passport-local"),
+LocalStrategy           = require("passport-local"),
 passportLocalMongoose   = require("passport-local-mongoose");
 
 mongoose.connect('mongodb://localhost:27017/auth_demo_app', {useNewUrlParser: true});
@@ -19,6 +19,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -43,7 +44,7 @@ app.get("/register", function(req, res) {
 res.render("register");
 });
 
-// R
+// Handling User Sign Up
 app.post("/register", function(req, res){
     User.register(new User({username: req.body.username}), req.body.password, function(err, user){
         if (err) {
@@ -54,6 +55,18 @@ app.post("/register", function(req, res){
             res.redirect("/secret");
         });
     });
+});
+
+// LOGIN Routes
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// Login Logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
 });
 
 app.listen(3000, function() {
