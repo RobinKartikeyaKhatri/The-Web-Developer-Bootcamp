9efa23fe78260a9c558daf23532a7534512ee4bd
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-21 13:47:42 +0530

    create a login form, and login logic

--- YelpCamp/v6/app.js
+++ YelpCamp/v6/app.js
@@ -151,6 +151,20 @@ app.post("/register", function(req, res){
     });
 });
 
+// Show Login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+// Handle Login logic
+app.post("/login", passport.authenticate("local", 
+{
+    successRedirect: "/campgrounds", 
+    failureRedirect: "/login"
+}), function(req, res) {
+    
+});
+
 app.listen(3000, function() {
     console.log("YelpCamp V3 Server Started!");
 });
