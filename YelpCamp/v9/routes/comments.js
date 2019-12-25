a176796929fbe8abda35df3d42915b17c08ef5bb
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-25 13:50:34 +0530

    Implement comment authorization

--- YelpCamp/v9/routes/comments.js
+++ YelpCamp/v9/routes/comments.js
@@ -48,7 +48,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 // Comment Edit Route
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if (err) {
             res.redirect("back");
@@ -59,7 +59,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // Comment Update Route
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if (err) {
             res.redirect("back");
@@ -70,7 +70,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // Comment Destroy Route
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if (err) {
             res.redirect("back");
@@ -80,6 +80,24 @@ router.delete("/:comment_id", function(req, res) {
     });
     
 });
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
 
 // Middleware
 function isLoggedIn(req, res, next){
