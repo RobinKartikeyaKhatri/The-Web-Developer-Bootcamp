9bf31af73dcaea6d523595b272635c274d32724a
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-26 16:09:41 +0530

    Add flash-messages in different parts of application

--- YelpCamp/v10/routes/index.js
+++ YelpCamp/v10/routes/index.js
@@ -19,10 +19,13 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if (err){
-            console.log(err);
-            return res.render("register");
+            // req.flash("error", err);
+            // return res.render("register");
+            req.flash("error", err.message);
+            return res.redirect("/register");
         } 
         passport.authenticate("local")(req, res, function() {
+            req.flash("success", "Welcome to YelpCamp " + user.username);
             res.redirect("/campgrounds");
         });
     });
@@ -49,12 +52,6 @@ router.get("/logout", function(req, res){
     res.redirect("/campgrounds");
 });
 
-// Middleware
-function isLoggedIn(req, res, next){
-    if (req.isAuthenticated()){
-        return next()
-    }
-    res.redirect("/login");
-}
+
 
 module.exports = router;
